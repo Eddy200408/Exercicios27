2d3bc054b2e0d66894148d3da62b4cdfd2d19719
Eddy200408 <edycoutinho04@gmail.com>
2024-03-27 19:03:25 -0100

    commit1

---
+++ src/Endereco.java
@@ -0,0 +1,9 @@
+public class Endereco {
+    private int numeroCasa;
+    private String rua;
+    private int numeroAP;
+    private String Cidade;
+    private int codigoPostal;
+
+    public void Endereco(int numeroCasa, String rua, int numeroAP, String Cidade)
+}
